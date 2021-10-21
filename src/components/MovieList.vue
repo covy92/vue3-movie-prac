516f8172d71e99492126530cd5a16ba179bb1962
covy <tjddlf1232@gmail.com>
2021-10-21 21:45:47 +0900

    검색기능 추가

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="container">
     <div class="inner">
+      <div class="message">
+        {{ message }}
+      </div>
       <MovieItem 
         v-for="movie in movies"
-        :key="movie.imdbID" />
+        :key="movie.imdbID" 
+        :movie="movie" />
     </div>
   </div>
 </template>
@@ -16,9 +20,12 @@ export default {
   components:{
     MovieItem
   },
-  data(){
-    return{
-      movies:[]
+  computed:{
+    movies(){
+      return this.$store.state.movie.movies
+    },
+    message(){
+      return this.$store.state.movie.movies
     }
   }
 }
